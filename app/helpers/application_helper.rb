c4a06efdd5c285b30d0bea85b7856d226bceef69
Raven Duran <raven@webnified.com>
2016-06-14 01:40:22 +0800

    Revised indention to align all

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,8 @@ module ApplicationHelper
 		hash = {}
 
 		array.each do |e|
-		  key_value = e.split(key_sep)
-		  hash[key_value[0].strip.gsub(/^{/,"")] = key_value[1].strip.gsub(/}$/,"")
+		  	key_value = e.split(key_sep)
+			hash[key_value[0].strip.gsub(/^{/,"")] = key_value[1].strip.gsub(/}$/,"")
 		end
 
 		hash
@@ -16,7 +16,7 @@ module ApplicationHelper
 	def pretty_format_changes(changes)
 	    pretty = ""
 	    changes.each do |key,value|
-	      pretty << "#{key}: \"#{value}\", "
+			pretty << "#{key}: \"#{value}\", "
 	    end
 	    pretty.strip.gsub(/,$/,"")
   	end
