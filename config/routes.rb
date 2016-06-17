86d7c7396148a88434a8a60ba51a7ef37538a193
Raven Duran <raven@webnified.com>
2016-06-18 00:15:28 +0800

    Removed GET for Search

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'object_records/index'
   get 'object_records/delete_all'
   root "object_records#index"
-  match "/search" => "object_records#search", via: [:get, :post]
+  match "/search" => "object_records#search", via: [:post]
   match "/import" => "object_records#import", via: [:post]
 
   # The priority is based upon order of creation: first created -> highest priority.
